6ab04f88d14a7fc888a1666f014fc7408dd0ea40
Nik Spyratos <17888779+nikolaos-spyratos@users.noreply.github.com>
2023-01-29 09:39:09 +0200

    WIP: BankZero CSV Parser

---
+++ app/Services/BankZeroCsvParser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+class BankZeroCsvParser
+{
+    private $parser;
+
+    public function __construct()
+    {
+
+    }
+
+    public function parseCsv(): array
+    {
+
+    }
+}

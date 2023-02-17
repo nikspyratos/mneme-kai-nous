0ee3b1d799202245b4a1977a860fbe75c3ced8b3
Nik Spyratos <17888779+nikolaos-spyratos@users.noreply.github.com>
2023-02-17 16:01:04 +0200

    Feat: Filament base install + Account & Transaction resources

---
+++ app/Filament/Resources/AccountResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AccountResource\Pages;
+use App\Filament\Resources\AccountResource\RelationManagers;
+use App\Models\Account;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AccountResource extends Resource
+{
+    protected static ?string $model = Account::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('bank_name')
+                    ->required(),
+                Forms\Components\TextInput::make('account_number')
+                    ->required(),
+                Forms\Components\TextInput::make('currency')
+                    ->required(),
+                Forms\Components\TextInput::make('balance')
+                    ->required(),
+                Forms\Components\TextInput::make('type')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('bank_name'),
+                Tables\Columns\TextColumn::make('account_number'),
+                Tables\Columns\TextColumn::make('currency'),
+                Tables\Columns\TextColumn::make('balance'),
+                Tables\Columns\TextColumn::make('type'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAccounts::route('/'),
+            'create' => Pages\CreateAccount::route('/create'),
+            'edit' => Pages\EditAccount::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ app/Filament/Resources/TransactionResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransactionResource\Pages;
+use App\Filament\Resources\TransactionResource\RelationManagers;
+use App\Models\Transaction;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TransactionResource extends Resource
+{
+    protected static ?string $model = Transaction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('account_id')
+                    ->relationship('account', 'name')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('date')
+                    ->required(),
+                Forms\Components\Textarea::make('description')
+                    ->required(),
+                Forms\Components\Textarea::make('detail'),
+                Forms\Components\TextInput::make('currency')
+                    ->required(),
+                Forms\Components\TextInput::make('amount')
+                    ->required(),
+                Forms\Components\TextInput::make('listed_balance')
+                    ->required(),
+                Forms\Components\Select::make('expense_id')
+                    ->relationship('expense', 'name'),
+                Forms\Components\Select::make('budget_id')
+                    ->relationship('budget', 'name'),
+                Forms\Components\Select::make('tally_id')
+                    ->relationship('tally', 'name'),
+                Forms\Components\TextInput::make('type'),
+                Forms\Components\TextInput::make('fee'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('account.name'),
+                Tables\Columns\TextColumn::make('date')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\TextColumn::make('detail'),
+                Tables\Columns\TextColumn::make('currency'),
+                Tables\Columns\TextColumn::make('amount'),
+                Tables\Columns\TextColumn::make('listed_balance'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('expense.name'),
+                Tables\Columns\TextColumn::make('budget.name'),
+                Tables\Columns\TextColumn::make('tally.name'),
+                Tables\Columns\TextColumn::make('type'),
+                Tables\Columns\TextColumn::make('fee'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransactions::route('/'),
+            'create' => Pages\CreateTransaction::route('/create'),
+            'edit' => Pages\EditTransaction::route('/{record}/edit'),
+        ];
+    }    
+}

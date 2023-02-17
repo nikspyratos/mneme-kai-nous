9361521a01e878ee9ed8aaaca1ffffc2c8d54f34
Nik Spyratos <17888779+nikolaos-spyratos@users.noreply.github.com>
2023-02-17 16:58:57 +0200

    Feat: Transactions -> Markdown Action stub

---
+++ app/Actions/ConvertTransactionsToMarkdown.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Actions;
+
+use Illuminate\Database\Eloquent\Collection;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ConvertTransactionsToMarkdown
+{
+    use AsAction;
+
+    public function handle(Collection $transactions)
+    {
+
+    }
+}

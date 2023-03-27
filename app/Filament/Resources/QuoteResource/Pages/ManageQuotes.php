16f0e8dd078e7538c41d2d6e215ac11c1a491553
Nik Spyratos <17888779+nikolaos-spyratos@users.noreply.github.com>
2023-03-27 23:22:12 +0200

    Feat: Mass quotes import

--- app/Filament/Resources/QuoteResource/Pages/ManageQuotes.php
+++ app/Filament/Resources/QuoteResource/Pages/ManageQuotes.php
@@ -3,17 +3,37 @@
 namespace App\Filament\Resources\QuoteResource\Pages;
 
 use App\Filament\Resources\QuoteResource;
+use App\Imports\QuotesImport;
+use Filament\Forms\Components\FileUpload;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ManageRecords;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ManageQuotes extends ManageRecords
 {
     protected static string $resource = QuoteResource::class;
 
+    public function importQuotes(array $data)
+    {
+        Excel::import(new QuotesImport, Storage::path($data['file']));
+    }
+
     protected function getActions(): array
     {
         return [
             Actions\CreateAction::make(),
+            Actions\Action::make('import')
+                ->label('Import quotes')
+                ->color('success')
+                ->icon('heroicon-o-document-add')
+                ->action('importQuotes')
+                ->form([
+                    FileUpload::make('file')
+                        ->disk(Storage::getDefaultDriver())
+                        ->label('File')
+                        ->rules('required'),
+                ]),
         ];
     }
 }
---
+++ app/Imports/QuotesImport.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Perception;
+use App\Models\Quote;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class QuotesImport implements ToModel, WithHeadingRow
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function model(array $row)
+    {
+        $perceptions = Perception::all();
+
+        return new Quote([
+            'perception_id' => $perceptions->firstWhere('slug', $row['perception'])->id,
+            'content' => $row['content'],
+            'author' => $row['author'],
+        ]);
+    }
+}

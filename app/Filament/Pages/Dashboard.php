990caa233e6d73d4df3a87284b3f8c5602582711
Nik Spyratos <17888779+nikolaos-spyratos@users.noreply.github.com>
2023-03-28 00:24:41 +0200

    WIP Feat: database download

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -3,11 +3,29 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Dashboard as BasePage;
+use Illuminate\Support\Facades\Storage;
 
 class Dashboard extends BasePage
 {
+    public function downloadDatabase()
+    {
+        //TODO Find a way to make this work
+        return Storage::disk('database')->download('database.sqlite');
+    }
+
     protected function getColumns(): int|array
     {
         return 5;
+    }
+
+    protected function getActions(): array
+    {
+        return [
+//            Filament\Pages\Actions\Action::make('download_database')
+//                ->label('Download Database')
+//                ->color('success')
+//                ->icon('heroicon-o-database')
+//                ->action('downloadDatabase'),
+        ];
     }
 }
--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,12 @@ return [
             'throw' => false,
         ],
 
+        'database' => [
+            'driver' => 'local',
+            'root' => database_path(''),
+            'throw' => false,
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),

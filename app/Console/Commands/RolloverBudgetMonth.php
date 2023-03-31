7427fa442cfe21c41909d09d338989bbbc1d85a8
Nik Spyratos <17888779+nikolaos-spyratos@users.noreply.github.com>
2023-03-31 21:14:28 +0200

    Feat: Remove Spent from tally names

--- app/Console/Commands/RolloverBudgetMonth.php
+++ app/Console/Commands/RolloverBudgetMonth.php
@@ -57,7 +57,7 @@ class RolloverBudgetMonth extends Command
             if ($shouldCreateTally) {
                 $tally = Tally::create([
                     'budget_id' => $budget->id,
-                    'name' => $budget->name . ' Spent ' . $startDate->toDateString() . ' - ' . $nextMonthDay->toDateString(),
+                    'name' => $budget->name . ' ' . $startDate->toDateString() . ' - ' . $nextMonthDay->toDateString(),
                     'currency' => $budget->currency,
                     'balance' => 0,
                     'limit' => $budget->amount,

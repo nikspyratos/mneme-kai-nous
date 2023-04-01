30e0508d45d8a0f111f1f962c1a682739375f4ac
Nik Spyratos <17888779+nikolaos-spyratos@users.noreply.github.com>
2023-04-01 16:21:57 +0200

    Feat: Transaction Creation fix amount

--- app/Filament/Resources/TransactionResource/Pages/CreateTransaction.php
+++ app/Filament/Resources/TransactionResource/Pages/CreateTransaction.php
@@ -4,8 +4,17 @@ namespace App\Filament\Resources\TransactionResource\Pages;
 
 use App\Filament\Resources\TransactionResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateTransaction extends CreateRecord
 {
     protected static string $resource = TransactionResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $data['amount'] *= 100;
+        $data['listed_balance'] *= 100;
+
+        return static::getModel()::create($data);
+    }
 }

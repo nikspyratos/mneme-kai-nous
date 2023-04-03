85a96d1f1e1d13fd9396da3b60754fa30ad7faee
Nik Spyratos <17888779+nikolaos-spyratos@users.noreply.github.com>
2023-04-03 22:45:34 +0200

    Log theme link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Filament::registerViteTheme('resources/css/app.css');
+        \Log::info(Filament::getThemeLink()->toHtml());
         Filament::serving(function () {
             Filament::registerNavigationGroups([
                 NavigationGroup::make()

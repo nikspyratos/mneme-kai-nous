ece3410cb1f14539751acec54ba059ee27328e35
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2023-08-02 11:08:25 +0200

    Feat: Deployer update for ubuntu on AWS

--- deploy.php
+++ deploy.php
@@ -8,23 +8,28 @@ require 'recipe/laravel.php';
 set('repository', 'git@github.com:nikspyratos/mneme-kai-nous.git');
 
 add('shared_files', []);
-add('shared_dirs', []);
+add('shared_dirs', ['database']);
 add('writable_dirs', []);
 
 // Hosts
 
-host('207.154.230.127')
-    ->set('remote_user', 'forge')
+host('13.246.221.228')
+    ->set('remote_user', 'deployer')
     ->set('deploy_path', '~/mneme-kai-nous.ankyr.dev')
-    ->set('ssh_arguments', ['-i ~/.ssh/id_rsa_mkn']);
+    ->set('ssh_arguments', ['-i ~/.ssh/mkn_aws.pem'])
+    ->set('forward_agent', false);
 
 task('composer', function () {
-    run('cd ~/mneme-kai-nous.ankyr.dev && composer install');
+    run('cd ~/mneme-kai-nous.ankyr.dev/current && composer install');
 });
 
 task('npm', function () {
-    run('cd ~/mneme-kai-nous.ankyr.dev && npm install');
-    run('cd ~/mneme-kai-nous.ankyr.dev && npm run build');
+    run('cd ~/mneme-kai-nous.ankyr.dev/current && npm install');
+    run('cd ~/mneme-kai-nous.ankyr.dev/current && npm run build');
+});
+
+task('cron:install', function() {
+    run('crontab ~/mneme-kai-nous.ankyr.dev/current/cron');
 });
 
 // Hooks

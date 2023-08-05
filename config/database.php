208b5b30d86d4aff47822bd107f698c231173dfe
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2023-08-05 18:02:19 +0200

    DB location fix attempt #5.1 the real one

--- config/database.php
+++ config/database.php
@@ -40,7 +40,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => base_path('database.sqlite'),
+            'database' => base_path('sqlite/database.sqlite'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
